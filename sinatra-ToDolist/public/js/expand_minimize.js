64c15b8519ee42dc6f1408d26626d8d7fe382f74
itggot-stefan-chan <stefan.chan@itggot.se>
2016-06-02 20:26:29 +0200

    19th
    Fixade javscriptet, börjat mobilanpassa

--- sinatra-ToDolist/public/js/expand_minimize.js
+++ sinatra-ToDolist/public/js/expand_minimize.js
@@ -1,4 +1,18 @@
-$('#button').click(function(){
-    console.log("!");
-    $('#wrapper').toggle();
-})
+$( document ).ready(function() {
+    $(".open").click(function () {
+
+        $open = $(this);
+        //getting the next element
+        $content = $open.next();
+        //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+        $content.slideToggle(500, function () {
+            //execute this after slideToggle is done
+            //change text of header based on visibility of content div
+            $open.text(function () {
+                //change text based on condition
+                return $content.is(":visible") ? "Collapse" : "Expand";
+            });
+        });
+
+    });
+});
